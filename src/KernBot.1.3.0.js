401bc284035eaf254a47a3f73f389f8220b68b76
grablej919 <grablej919@gmail.com>
2018-02-27 16:24:58 -0800

    made styles mobile responsive

--- src/KernBot.1.3.0.js
+++ src/KernBot.1.3.0.js
@@ -1,10 +1,10 @@
 /*
 
 Author: Joey Grable
-Version: 1.3.0
+Version: 1.3.X
 GIT: github.com/joeygrable94/KernBot
 
-A javascript library that dynamically kerns characters based on their font size.
+A javascript library that dynamically kerns characters based on their context.
 KernBot uses traditional calligraphy methods to categorize letters by the types
 of letter strokes they are comprised of. It then calculates the relative value
 letter-spacing by comparing the character's stroke types to the adjacent letters.
